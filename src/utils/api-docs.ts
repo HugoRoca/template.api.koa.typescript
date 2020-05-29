c5d5b02319aca1013b6b204e208363f9441d1fc9
hugoroca <hugo.rock20@hotmail.com>
2020-05-28 22:25:06 -0500

    algunos fixs

--- src/utils/api-docs.ts
+++ src/utils/api-docs.ts
@@ -3,6 +3,6 @@ import * as swagger from 'swagger2'
 import { ui } from 'swagger2-koa'
 
 const file = path.join(__dirname, 'docs.yml')
-const document = swagger.loadDocumentSync(file)
+const document: any = swagger.loadDocumentSync(file)
 
 export default ui(document, '/docs')
